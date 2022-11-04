874323fc7e60030aebeb0b78cbd2254b6ea5481f
etqasik <117316719+etqasik@users.noreply.github.com>
2022-11-05 00:06:04 +0300

    bd_KURSA4
    )

---
+++ Dump20221104.sql
@@ -0,0 +1,154 @@
+CREATE DATABASE  IF NOT EXISTS `kursa4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `kursa4`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: kursa4
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mappool`
+--
+
+DROP TABLE IF EXISTS `mappool`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mappool` (
+  `IdMap` int NOT NULL AUTO_INCREMENT,
+  `MapName` text NOT NULL,
+  PRIMARY KEY (`IdMap`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mappool`
+--
+
+LOCK TABLES `mappool` WRITE;
+/*!40000 ALTER TABLE `mappool` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mappool` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `IdPlayers` int NOT NULL,
+  `NickPlayer` text NOT NULL,
+  PRIMARY KEY (`IdPlayers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statsplayers`
+--
+
+DROP TABLE IF EXISTS `statsplayers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statsplayers` (
+  `Player` int NOT NULL,
+  `KD` float NOT NULL,
+  `KR` float NOT NULL,
+  `AVGKILLS` float NOT NULL,
+  `EloRankedPoints` int NOT NULL,
+  `Lvl` int NOT NULL,
+  PRIMARY KEY (`Player`),
+  CONSTRAINT `StatsPlayers_fk0` FOREIGN KEY (`Player`) REFERENCES `players` (`IdPlayers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statsplayers`
+--
+
+LOCK TABLES `statsplayers` WRITE;
+/*!40000 ALTER TABLE `statsplayers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `statsplayers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams` (
+  `IdCaptain` int NOT NULL,
+  `TeamName` text NOT NULL,
+  PRIMARY KEY (`IdCaptain`),
+  CONSTRAINT `Teams_fk0` FOREIGN KEY (`IdCaptain`) REFERENCES `players` (`IdPlayers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teams`
+--
+
+LOCK TABLES `teams` WRITE;
+/*!40000 ALTER TABLE `teams` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `winrateonmaps`
+--
+
+DROP TABLE IF EXISTS `winrateonmaps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `winrateonmaps` (
+  `Map` int NOT NULL,
+  `PlayerId` int NOT NULL,
+  `WinRate` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`Map`),
+  KEY `WinRateOnMaps_fk1` (`PlayerId`),
+  CONSTRAINT `WinRateOnMaps_fk0` FOREIGN KEY (`Map`) REFERENCES `mappool` (`IdMap`),
+  CONSTRAINT `WinRateOnMaps_fk1` FOREIGN KEY (`PlayerId`) REFERENCES `players` (`IdPlayers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `winrateonmaps`
+--
+
+LOCK TABLES `winrateonmaps` WRITE;
+/*!40000 ALTER TABLE `winrateonmaps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `winrateonmaps` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-04 23:52:07
